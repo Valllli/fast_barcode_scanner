0d0ac52a8a6990ac741548d80b0eae3716737acb
Valllli <pendurin@fittin.com>
2024-10-23 12:58:44 +0300

    fix

--- fast_barcode_scanner/example/README.md
+++
@@ -1,3 +0,0 @@
-# fast_barcode_scanner_example
-
-Demonstrates how to use the fast_barcode_scanner plugin.
--- fast_barcode_scanner/example/android/app/src/main/kotlin/com/jhoogstraat/fast_barcode_scanner_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.jhoogstraat.fast_barcode_scanner_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
